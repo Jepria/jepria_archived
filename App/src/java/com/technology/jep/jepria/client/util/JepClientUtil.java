7574e590e0ad867abc33e9153279a6567eb54293
almirus <almir.abrarov@gmail.com>
2016-01-22 11:50:42 +0300

    * fix(JepRia): goToUrl добавлен JavaDoc, добавлена возможность перехода по FTP * fix(JepRia): getElementValue теперь возвращает пустую строку если есть value у dom элемента * fix(JepRia): везде (и в чужих методах) заменены пробелы на табляцию

--- App/src/java/com/technology/jep/jepria/client/util/JepClientUtil.java
+++ App/src/java/com/technology/jep/jepria/client/util/JepClientUtil.java
@@ -27,40 +27,44 @@ public class JepClientUtil {
 	private static final String LOADING_HEADER_ID = "loadingHeader";
 	private static final String LOADING_MESSAGE_ID = "loadingMessage";
 	private static final String DISABLED_LAYER_ID = "disabledLayerId";
-	
+
 	private static final String DISABLED_LAYER_STYLE = "jepRia-disabledLayer";
-	
+
 	public static final BodyElement BODY = Document.get().getBody();
-	
+
 	public static native String getUserAgent() /*-{
 		return navigator.userAgent.toLowerCase();
 	}-*/;
-	
+
+	/**
+	 * Функция для перехода по ссылке
+	 * @param url	ссылка в любов формате (прямая, относительная, без указания протокола)
+	 */
 	public static void goToUrl(String url) {
-        if (url.startsWith("//")){
-            //absolute url without protocol
-            url = Window.Location.getProtocol() + url;
-        } else if (!url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("mailto:")){
-            //relative path
-            url = GWT.getHostPageBaseURL() + url.replaceFirst("/","");
-        }
-        //absolute url
-        Window.Location.assign(url);
+		if (url.startsWith("//")){
+			//absolute url without protocol
+			url = Window.Location.getProtocol() + url;
+		} else if (!url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("mailto:") && !url.startsWith("ftp:")){
+			//relative path
+			url = GWT.getHostPageBaseURL() + url.replaceFirst("/","");
+		}
+		//absolute url
+		Window.Location.assign(url);
 	}
 
 	/**
 	 * Показ загрузочной панели с предустановленным заголовком и сообщением
-	 * 
+	 *
 	 * @param header		заголовок (меняется, если он установлен)
 	 * @param message		описательное сообщение (меняется, если он установлен)
 	 */
 	public static void showLoadingPanel(String header, String message) {
 		showLoadingPanel(null, header, message);
 	}
-	
+
 	/**
 	 * Показ загрузочной панели с предустановленным заголовком и сообщением
-	 * 
+	 *
 	 * @param layerId		идентификатор слоя блокировки
 	 * @param header		заголовок (меняется, если он установлен)
 	 * @param message		описательное сообщение (меняется, если он установлен)
@@ -70,20 +74,20 @@ public class JepClientUtil {
 		Element loadingHeader = DOM.getElementById(LOADING_HEADER_ID);
 		Element loadingMessage = DOM.getElementById(LOADING_MESSAGE_ID);
 		if (loading != null) {
-			
+
 			if (!JepRiaUtil.isEmpty(layerId)){
 				loading = DOM.clone(loading, true);
 				loading.setId(generateElementLayerId(LOADING_PANEL_ID, layerId));
-				
+
 				loadingHeader = getNestedElementById(loading, LOADING_HEADER_ID);
 				loadingHeader.setId(generateElementLayerId(LOADING_HEADER_ID, layerId));
-				
+
 				loadingMessage = getNestedElementById(loading, LOADING_MESSAGE_ID);
 				loadingMessage.setId(generateElementLayerId(LOADING_MESSAGE_ID, layerId));
-				
+
 				BODY.appendChild(loading);
 			}
-			
+
 			loading.getStyle().setDisplay(Display.BLOCK);
 			appendDisabledLayer(layerId);
 			if (!JepRiaUtil.isEmpty(header)) {
@@ -94,10 +98,10 @@ public class JepClientUtil {
 			}
 		}
 	}
-	
+
 	/**
 	 * Поиск элемента по его идентификатору относительно родительского.
-	 * 
+	 *
 	 * @param parent		родительский элемент в DOM-дереве
 	 * @param id			идентификатор искомого элемента
 	 * @return элемент в DOM-дереве
@@ -124,40 +128,40 @@ public class JepClientUtil {
 	private static Node getDisabledLayer(String layerId) {
 		String disabledLayerId = generateElementLayerId(DISABLED_LAYER_ID, layerId);
 		Element disabledLayer = DOM.getElementById(disabledLayerId);
-		
+
 		if(disabledLayer == null) {
 			disabledLayer = DOM.createDiv();
 			disabledLayer.setId(disabledLayerId);
-			
+
 			Style disabledLayerStyle = disabledLayer.getStyle();
 			disabledLayer.addClassName(DISABLED_LAYER_STYLE);
 			disabledLayerStyle.setZIndex(99);
 		}
-		
+
 		return disabledLayer;
 	}
-	
+
 	/**
 	 * Генерация уникального идентификатора элемента.
-	 * 
-	 * @param elementId			идентификатор элемента 
+	 *
+	 * @param elementId			идентификатор элемента
 	 * @param layerId			идентификатор слоя
 	 * @return уникальный идентификатор
 	 */
 	private static final String generateElementLayerId(String elementId, String layerId){
 		return elementId + (layerId == null ? "" : ("_" + layerId));
 	}
-	
+
 	/**
 	 * Скрытие загрузочной панели
 	 */
 	public static void hideLoadingPanel(){
 		hideLoadingPanel(null);
 	}
-	
+
 	/**
 	 * Скрытие загрузочной панели
-	 * 
+	 *
 	 * @param layerId		идентификатор слоя блокировки
 	 */
 	public static void hideLoadingPanel(final String layerId){
@@ -177,30 +181,30 @@ public class JepClientUtil {
 			t.schedule(500);
 		}
 	}
-	
+
 	/**
 	 * Создание блокирующего страницу слоя
-	 * 
+	 *
 	 * @param layerId		идентификатор слоя блокировки
 	 */
-	public static void appendDisabledLayer(String layerId) {		
-		BODY.appendChild(getDisabledLayer(layerId));	   
+	public static void appendDisabledLayer(String layerId) {
+		BODY.appendChild(getDisabledLayer(layerId));
 	}
 
 	/**
 	 * Удаление блокирующего страницу слоя
-	 * 
+	 *
 	 * @param layerId		идентификатор слоя блокировки
 	 */
 	public static void removeDisabledLayer(final String layerId) {
 		Timer t = new Timer() {
-		    public void run() {
-		       	if (!JepRiaUtil.isEmpty(DOM.getElementById(generateElementLayerId(DISABLED_LAYER_ID, layerId)))){
-		       		BODY.removeChild(getDisabledLayer(layerId));
-		       	}
-		    }
+			public void run() {
+				if (!JepRiaUtil.isEmpty(DOM.getElementById(generateElementLayerId(DISABLED_LAYER_ID, layerId)))){
+					BODY.removeChild(getDisabledLayer(layerId));
+				}
+			}
 		};
-	    t.schedule(500);
+		t.schedule(500);
 	}
 
 	public static void addForeignKey(JepRecord record) {
@@ -214,147 +218,147 @@ public class JepClientUtil {
 	public static JepWorkstatePlace workstateToPlace(WorkstateEnum workstate) {
 		JepWorkstatePlace place = null;
 		switch(workstate) {
-		case CREATE:
-			place = new JepCreatePlace();
-			break;
-		case EDIT:
-			place = new JepEditPlace();
-			break;
-		case SEARCH:
-			place = new JepSearchPlace();
-			break;
-		case SELECTED:
-			place = new JepSelectedPlace();
-			break;
-		case VIEW_DETAILS:
-			place = new JepViewDetailPlace();
-			break;
-		case VIEW_LIST:
-			place = new JepViewListPlace();
-			break;
+			case CREATE:
+				place = new JepCreatePlace();
+				break;
+			case EDIT:
+				place = new JepEditPlace();
+				break;
+			case SEARCH:
+				place = new JepSearchPlace();
+				break;
+			case SELECTED:
+				place = new JepSelectedPlace();
+				break;
+			case VIEW_DETAILS:
+				place = new JepViewDetailPlace();
+				break;
+			case VIEW_LIST:
+				place = new JepViewListPlace();
+				break;
 		}
 		return place;
 	}
 
 	/**
-	   * Substitutes the indexed parameters.
-	   * 
-	   * @param text the text
-	   * @param params the parameters
-	   * @return the new text
-	   */
-	  public static String substitute(String text, Object... params) {
-	    for (int i = 0; i < params.length; i++) {
-	      Object p = params[i];
-	      if (p == null) {
-	        p = "";
-	      }
-	      text = text.replaceAll("\\{" + i + "}", safeRegexReplacement(p.toString()));
-	    }
-	    return text;
-	  }
-	  
-	  /**
-	   * Replace any \ or $ characters in the replacement string with an escaped \\
-	   * or \$.
-	   * 
-	   * @param replacement the regular expression replacement text
-	   * @return null if replacement is null or the result of escaping all \ and $
-	   *         characters
-	   */
-	  private static String safeRegexReplacement(String replacement) {
-	    if (replacement == null) {
-	      return replacement;
-	    }
+	 * Substitutes the indexed parameters.
+	 *
+	 * @param text the text
+	 * @param params the parameters
+	 * @return the new text
+	 */
+	public static String substitute(String text, Object... params) {
+		for (int i = 0; i < params.length; i++) {
+			Object p = params[i];
+			if (p == null) {
+				p = "";
+			}
+			text = text.replaceAll("\\{" + i + "}", safeRegexReplacement(p.toString()));
+		}
+		return text;
+	}
 
-	    return replacement.replaceAll("\\\\", "\\\\\\\\").replaceAll("\\$", "\\\\\\$");
-	  }
-	  
+	/**
+	 * Replace any \ or $ characters in the replacement string with an escaped \\
+	 * or \$.
+	 *
+	 * @param replacement the regular expression replacement text
+	 * @return null if replacement is null or the result of escaping all \ and $
+	 * characters
+	 */
+	private static String safeRegexReplacement(String replacement) {
+		if (replacement == null) {
+			return replacement;
+		}
 
-	 /**
-	  * Intended to be used to pull the value out of a CSS length. If the
-	  * value is "auto" or "inherit", 0 will be returned.
-	  * 
-	  * @param s The CSS length string to extract
-	  * @return The leading numeric portion of <code>s</code>, or 0 if "auto" or
-	  *         "inherit" are passed in.
-	  */
-	 public static native double extractLengthValue(String s) /*-{
-	    if (s == "auto" || s == "inherit" || s == "") {
-	      return 0;
-	    } else {
-	      // numberRegex is similar to java.lang.Number.floatRegex, but divides
-	      // the string into a leading numeric portion followed by an arbitrary
-	      // portion.
-	      var numberRegex = @com.google.gwt.user.client.ui.UIObject::numberRegex;
-	      if (!numberRegex) {
-	        numberRegex = @com.google.gwt.user.client.ui.UIObject::numberRegex =
-	          /^(\s*[+-]?((\d+\.?\d*)|(\.\d+))([eE][+-]?\d+)?)(.*)$/;
-	      }
+		return replacement.replaceAll("\\\\", "\\\\\\\\").replaceAll("\\$", "\\\\\\$");
+	}
 
-	      // Extract the leading numeric portion of s
-	      s = s.replace(numberRegex, "$1");
-	      return parseFloat(s);
-	    }
-	 }-*/;
-	 
-	 /**
-	   * Проверяет: являтся ли нажатая кнопка "специальной".<br/>
-	   * <br/>
-	   * В качестве реализации используется одноименная функция параметра event, а также осуществляется проверка нажатия клавиши Ctrl.<br/>
-	   * Добавлена проверка на клавишу Print Screen.<br/>
-	   * 
-	   * @param event событие, срабатываемое браузером
-	   * @return флаг является ли нажатая кнопка "специальной" или Ctrl
-	   */
-	 public static boolean isSpecialKey(DomEvent<?> event){
+
+	/**
+	 * Intended to be used to pull the value out of a CSS length. If the
+	 * value is "auto" or "inherit", 0 will be returned.
+	 *
+	 * @param s The CSS length string to extract
+	 * @return The leading numeric portion of <code>s</code>, or 0 if "auto" or
+	 * "inherit" are passed in.
+	 */
+	public static native double extractLengthValue(String s) /*-{
+		if (s == "auto" || s == "inherit" || s == "") {
+			return 0;
+		} else {
+// numberRegex is similar to java.lang.Number.floatRegex, but divides
+// the string into a leading numeric portion followed by an arbitrary
+// portion.
+			var numberRegex = @com.google.gwt.user.client.ui.UIObject::numberRegex;
+			if (!numberRegex) {
+				numberRegex = @com.google.gwt.user.client.ui.UIObject::numberRegex =
+					/^(\s*[+-]?((\d+\.?\d*)|(\.\d+))([eE][+-]?\d+)?)(.*)$/;
+			}
+
+// Extract the leading numeric portion of s
+			s = s.replace(numberRegex, "$1");
+			return parseFloat(s);
+		}
+	}-*/;
+
+	/**
+	 * Проверяет: являтся ли нажатая кнопка "специальной".<br/>
+	 * <br/>
+	 * В качестве реализации используется одноименная функция параметра event, а также осуществляется проверка нажатия клавиши Ctrl.<br/>
+	 * Добавлена проверка на клавишу Print Screen.<br/>
+	 *
+	 * @param event событие, срабатываемое браузером
+	 * @return флаг является ли нажатая кнопка "специальной" или Ctrl
+	 */
+	public static boolean isSpecialKey(DomEvent<?> event){
 		int keyCode = event.getNativeEvent().getKeyCode();
 		return isSpecialKey(keyCode) || event.getNativeEvent().getCtrlKey() || keyCode == 44; //Блокируем реакцию на клавишу PrintScreen, код 44
-	 }
-	
-	 /**
-	  * Returns true if the key is a "special" key.
-	  * 
-	  * @param k the key code
-	  * @return the special state
-	  */
-	 public static boolean isSpecialKey(int k) {
-	    return isNavKeyPress(k) || k == KeyCodes.KEY_BACKSPACE || k == KeyCodes.KEY_CTRL || k == KeyCodes.KEY_SHIFT
-	        || k == KeyCodes.KEY_ALT || (k >= 19 && k <= 20) || (k >= 45 && k <= 46);
-	 }
-	 
-	 /**
-	  * Returns true if the key is a "navigation" key.
-	  * 
-	  * @param k the key code
-	  * @return the nav state
-	  */
-	 public static boolean isNavKeyPress(int k) {
+	}
+
+	/**
+	 * Returns true if the key is a "special" key.
+	 *
+	 * @param k the key code
+	 * @return the special state
+	 */
+	public static boolean isSpecialKey(int k) {
+		return isNavKeyPress(k) || k == KeyCodes.KEY_BACKSPACE || k == KeyCodes.KEY_CTRL || k == KeyCodes.KEY_SHIFT
+				|| k == KeyCodes.KEY_ALT || (k >= 19 && k <= 20) || (k >= 45 && k <= 46);
+	}
+
+	/**
+	 * Returns true if the key is a "navigation" key.
+	 *
+	 * @param k the key code
+	 * @return the nav state
+	 */
+	public static boolean isNavKeyPress(int k) {
 		return (k >= 33 && k <= 40) || k == KeyCodes.KEY_ESCAPE || k == KeyCodes.KEY_ENTER || k == KeyCodes.KEY_TAB;
-	 }
-	 
-	 // needed due to GWT 2.1 changes
-	 /**
-	  * Получение кода вводимого с клавиатуры символа <br/>
-	  * (актуально для случаев событий onKeyUp - разные браузеры по-разному распознают код символа, в событиях же onKeyDown - коды символов приходят корректно).
-	  * 
-	  * @param e срабатываемое событие
-	  * @return представление кода вводимого символа, приведенного к типу char
-	  */
-	 public static native char getChar(NativeEvent e) /*-{
+	}
+
+	// needed due to GWT 2.1 changes
+	/**
+	 * Получение кода вводимого с клавиатуры символа <br/>
+	 * (актуально для случаев событий onKeyUp - разные браузеры по-разному распознают код символа, в событиях же onKeyDown - коды символов приходят корректно).
+	 *
+	 * @param e срабатываемое событие
+	 * @return представление кода вводимого символа, приведенного к типу char
+	 */
+	public static native char getChar(NativeEvent e) /*-{
 		return e.which || e.charCode || e.keyCode || 0;
-	 }-*/;
-	 
-	 /**
-	  * Поддержание активного элемента списка видимым при прокручивании списка мышью.<br/>
-	  * 
-	  * Если в выпадающем меню содержится такое количество опций, что пользователю приходится прокручивать список мышью, 
-	  * то возникает необходимость выравнивать элемент по высоте так, чтобы активный элемент списка стал видимым.<br/>
-	  * Данная операция используется в таких виджетах, как {@link ComboBox} и {@link GridHeaderMenuBar}.
-	  * 
-	  * @param elem	активный элемент, подвергаемый выравниванию
-	  */
-	 public static native void adjustToTop(Element elem) /*-{
+	}-*/;
+
+	/**
+	 * Поддержание активного элемента списка видимым при прокручивании списка мышью.<br/>
+	 *
+	 * Если в выпадающем меню содержится такое количество опций, что пользователю приходится прокручивать список мышью,
+	 * то возникает необходимость выравнивать элемент по высоте так, чтобы активный элемент списка стал видимым.<br/>
+	 * Данная операция используется в таких виджетах, как {@link ComboBox} и {@link GridHeaderMenuBar}.
+	 *
+	 * @param elem	активный элемент, подвергаемый выравниванию
+	 */
+	public static native void adjustToTop(Element elem) /*-{
 		var left = elem.offsetLeft, top = elem.offsetTop;
 		var width = elem.offsetWidth, height = elem.offsetHeight;
 
@@ -381,11 +385,11 @@ public class JepClientUtil {
 			cur = cur.parentNode;
 		}
 	}-*/;
-	 
+
 	/**
-	 * String.trim() is limited for using in all cases.	
-	 * Particularly, it doesn't include such html-entity as &nbsp (\u00a0), 
-	 * To solve that, add the unicode to the RegEx. 
+	 * String.trim() is limited for using in all cases.
+	 * Particularly, it doesn't include such html-entity as &nbsp (\u00a0),
+	 * To solve that, add the unicode to the RegEx.
 	 * @param s		string to trim
 	 * @return  trimmed string
 	 */
@@ -393,18 +397,18 @@ public class JepClientUtil {
 		return s.replace(/^[\s,\u00a0]+|[\s,\u00a0]+$/g, '');
 	}-*/;
 
-    /**
-     * Получаем значение по переданному id
-     * @param id в DOM
-     * @return значение в виде строки
-     */
-    private static native String getElementValue(String id) /*-{
-        var value = null;
-        var elementById = $wnd.document.getElementById(id);
-        if(elementById && elementById.value) {
-            value = elementById.value;
-        }
-        return value;
-    }-*/;
+	/**
+	 * Получаем значение по переданному id
+	 * @param id в DOM
+	 * @return значение в виде строки, если элемент не найден возвращается null, если элемент пустой возращается пустая строка
+	 */
+	private static native String getElementValue(String id) /*-{
+		var value = null;
+		var elementById = $wnd.document.getElementById(id);
+		if(elementById && elementById.value !== undefined) {
+			value = elementById.value;
+		}
+		return value;
+	}-*/;
 
 }
