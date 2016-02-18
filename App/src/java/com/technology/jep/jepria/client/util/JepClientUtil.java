6613f7f97ea8e50bfa2156a78f6289243ef10c01
almirus <almir.abrarov@gmail.com>
2016-02-18 16:52:39 +0300

    fix(JepRia): goToUrl теперь не учитывает регистр

--- App/src/java/com/technology/jep/jepria/client/util/JepClientUtil.java
+++ App/src/java/com/technology/jep/jepria/client/util/JepClientUtil.java
@@ -38,13 +38,13 @@ public class JepClientUtil {
 
 	/**
 	 * Функция для перехода по ссылке
-	 * @param url	ссылка в любов формате (прямая, относительная, без указания протокола)
+	 * @param url	ссылка в любом формате (прямая, относительная, без указания протокола)
 	 */
 	public static void goToUrl(String url) {
 		if (url.startsWith("//")){
 			//absolute url without protocol
 			url = Window.Location.getProtocol() + url;
-		} else if (!url.matches("(?i)^((https?|ftp|file)://|mailto:)")){
+		} else if (!url.matches("(?i)^((https?|ftp|file)://|mailto:).*")){
 			//relative path
 			url = GWT.getHostPageBaseURL() + url.replaceFirst("/","");
 		}
