8c263181e1f795d0751b78d57c130004a244912c
eydlin <a.aidlin@gmail.com>
2016-03-10 09:30:39 +0300

    refactor(JepRia): Удалён неиспользуемый функционал поддержки версионности.

--- App/src/java/com/technology/jep/jepria/server/version/ejb/JepVersionLocal.java
+++
@@ -1,8 +0,0 @@
-package com.technology.jep.jepria.server.version.ejb;
-
-import javax.ejb.Local;
-
-@Local
-public interface JepVersionLocal extends JepVersion {
-
-}
--- App/src/java/com/technology/jep/jepria/server/version/ejb/JepVersionRemote.java
+++
@@ -1,8 +0,0 @@
-package com.technology.jep.jepria.server.version.ejb;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface JepVersionRemote extends JepVersion {
-
-}
