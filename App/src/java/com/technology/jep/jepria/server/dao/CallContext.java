f88afc6e31cdae25f84475b744f4502953ee40df
eydlin <a.aidlin@gmail.com>
2016-04-01 10:15:18 +0300

    refactor(JepRia): Удаление неиспользуемого кода из CallContext.

--- App/src/java/com/technology/jep/jepria/server/dao/CallContext.java
+++ App/src/java/com/technology/jep/jepria/server/dao/CallContext.java
@@ -2,10 +2,7 @@ package com.technology.jep.jepria.server.dao;
 
 import java.sql.SQLException;
 
-import com.google.gwt.user.server.rpc.UnexpectedException;
 import com.technology.jep.jepria.server.db.Db;
-import com.technology.jep.jepria.shared.exceptions.ApplicationException;
-import com.technology.jep.jepria.shared.exceptions.SystemException;
 
 /**
  * Класс содержит функциональность поддержки EJB-вызовов
@@ -14,7 +11,6 @@ public class CallContext {
 	private static ThreadLocal<CallContext> context = new ThreadLocal<CallContext>();
 	
 	private Db db = null;
-	private Integer currentUserId = null;
 	
 	public CallContext(String dataSourceJndiName) {
 		db = new Db(dataSourceJndiName, false);
@@ -52,35 +48,6 @@ public class CallContext {
 		CallContext result = (CallContext) context.get();
 		context.set(null);
 		return result;
-	}
-
-//	/**
-//	 * Обёртка для исключения. Используется при желании добавить к исходному исключению дополнительную информацию.
-//	 * 
-//	 * @param message сообщение
-//	 * @param ex исходное исключение
-//	 */
-//	public static void processException(String message, Exception ex) throws ApplicationException {
-//		CallContext.processException(new ApplicationException(message, ex));
-//	}
-//
-//	public static void processException(Exception ex) throws ApplicationException {
-//		CallContext c = (CallContext) context.get();
-//		c.sessionContext.setRollbackOnly(); // В случае Exception делаем откат транзакции
-//		if (ex instanceof ApplicationException) {
-//			// TODO обработать исключение
-//			throw (ApplicationException) ex;
-//		} else if (ex instanceof SystemException) {
-//			// TODO обработать исключение
-//			throw (SystemException) ex;
-//		} else {
-//			throw new UnexpectedException("Unknown exception", ex);
-//		}
-//	}
-
-	public static Integer getCurrentUserId() {
-		CallContext c = (CallContext) context.get();
-		return c.currentUserId;
 	}
 	
 	public static void commit() throws SQLException {
