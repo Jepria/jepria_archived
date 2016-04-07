1d80251381f6711824cdfb3c8c853431380d6dbe
eydlin <a.aidlin@gmail.com>
2016-04-07 12:15:53 +0300

    fix(JepRia): Исправлены и задокументированы аннотации @Before и @After.
    Из-за того, что они не имели аннотакции
    @RetentionPolicy(RetentionPolicy.RUNTIME), во время исполнения они
    игнорировались. Кроме того, уточнена их применимость - только к методам:
    @Target(ElementType.METHOD).

--- App/src/java/com/technology/jep/jepria/server/dao/transaction/annotation/After.java
+++ App/src/java/com/technology/jep/jepria/server/dao/transaction/annotation/After.java
@@ -1,7 +1,38 @@
 package com.technology.jep.jepria.server.dao.transaction.annotation;
 
+import java.lang.annotation.*;
+
 import com.technology.jep.jepria.server.dao.transaction.handler.EndTransactionHandler;
 
+/**
+ * Позволяет указать кастомный обработчик начала транзакции.<br/>
+ * К примеру, кастомный обработчик может использоваться, чтобы
+ * сообщить в логе о завершении транзакции:
+ * <pre>public class CustomDao extends JepDao implements Custom {
+ *
+ *  private static final Logger log = Logger.getLogger(CustomDao.class);
+ *
+ *  public static class CustomEndTransactionHandler extends EndTransactionHandlerImpl{
+ *    {@literal @}Override
+ *    public void handle(Throwable caught) throws ApplicationException {
+ *      log.trace("Конец транзакции");
+ *      super.handle(caught);
+ *    }
+ *  };
+ *
+ *  {@literal @}Override
+ *  {@literal @}After(endTransactionHandler = CustomEndTransactionHandler.class)
+ *  public void transactionMethod() throws ApplicationException {    
+ *    . . .  
+ *  }
+ *}</pre>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface After {
+	/**
+	 * Возвращает обработчик начала транзакции.
+	 * @return экземпляр обработчика
+	 */
 	Class<? extends EndTransactionHandler> endTransactionHandler();
 }
--- App/src/java/com/technology/jep/jepria/server/dao/transaction/annotation/Before.java
+++ App/src/java/com/technology/jep/jepria/server/dao/transaction/annotation/Before.java
@@ -1,7 +1,38 @@
 package com.technology.jep.jepria.server.dao.transaction.annotation;
 
+import java.lang.annotation.*;
+
 import com.technology.jep.jepria.server.dao.transaction.handler.StartTransactionHandler;
 
+/**
+ * Позволяет указать кастомный обработчик завершения транзакции.
+ * К примеру, кастомный обработчик может использоваться, чтобы
+ * сообщить в логе о начале транзакции:
+ * <pre>public class CustomDao extends JepDao implements Custom {
+ *
+ *  private static final Logger log = Logger.getLogger(CustomDao.class);
+ *
+ *  public static class CustomStartTransactionHandler implements StartTransactionHandler{
+ *    {@literal @}Override
+ *    public void handle(String dataSourceJndiName) throws ApplicationException {
+ *      CallContext.begin(dataSourceJndiName);
+ *      log.trace("Начало транзакции");
+ *    }
+ *  };
+ *
+ *  {@literal @}Override
+ *  {@literal @}Before(startTransactionHandler = CustomStartTransactionHandler.class))
+ *  public void transactionMethod() throws ApplicationException {    
+ *    . . .  
+ *  }
+ *}</pre>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Before {
+	/**
+	 * Возвращает обработчик завершения транзакции.
+	 * @return обработчик завершения
+	 */
 	Class<? extends StartTransactionHandler> startTransactionHandler();
 }
