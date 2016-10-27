2d77ce289134aff3e8acbb1a2b08d8f69fdce645
remitot <call9096463086@gmail.com>
2016-10-27 15:32:33 +0300

    refactor(JepRia): indentation fixes

--- App/auto/java/com/technology/jep/jepria/auto/pages/FramePage.java
+++ App/auto/java/com/technology/jep/jepria/auto/pages/FramePage.java
@@ -11,84 +11,70 @@ import com.technology.jep.jepria.auto.util.WebDriverFactory;
 
 public abstract class FramePage extends AbstractPage {
 
-    @FindBy(name = "navigation")
-    public WebElement navigation;
+  @FindBy(name = "navigation")
+  public WebElement navigation;
 
-    @FindBy(name = "content")
-    public WebElement content;
+  @FindBy(name = "content")
+  public WebElement content;
 
-    @FindBy(xpath = "//a[contains(@href,'switchLanguage')]")
-    private WebElement locale;
+  @FindBy(xpath = "//a[contains(@href,'switchLanguage')]")
+  private WebElement locale;
 
-    public enum Locale {
-        RU, ENG;
+  public enum Locale {
+      RU, ENG;
+  }
+
+  @Override
+  public FramePage ensurePageLoaded() {
+    WebDriverFactory.getDriver().switchTo().defaultContent();
+    getWait().until(presenceOfElementLocated(By.xpath("//frameset")));
+
+    if (verifyLocale(Locale.ENG)) {
+      return switchLocale().ensurePageLoaded();
     }
 
-    @Override
-    public FramePage ensurePageLoaded() {
-        WebDriverFactory.getDriver().switchTo().defaultContent();
-        getWait().until(presenceOfElementLocated(By.xpath("//frameset")));
+    WebDriverFactory.getDriver().switchTo().defaultContent();
+    return this;
+  }
 
-        if (verifyLocale(Locale.ENG)) {
-            return switchLocale().ensurePageLoaded();
-        }
+  public FramePage getNavigation() {
+    WebDriverFactory.getDriver()
+        .switchTo()
+        .defaultContent()
+        .switchTo()
+        .frame(navigation);
+    return this;
+  }
 
-        WebDriverFactory.getDriver().switchTo().defaultContent();
-        return this;
-    }
-
-    public FramePage getNavigation() {
-      WebDriverFactory.getDriver()
-                .switchTo()
-                .defaultContent()
-                .switchTo()
-                .frame(navigation);
-        return this;
-    }
-
-    public FramePage getContent() {
-      WebDriverFactory.getDriver()
-                .switchTo()
-                .defaultContent()
-                .switchTo()
-                .frame(content);
-        return this;
-    }
+  public FramePage getContent() {
+    WebDriverFactory.getDriver()
+        .switchTo()
+        .defaultContent()
+        .switchTo()
+        .frame(content);
+    return this;
+  }
     
-    
-/*
-    public ArrayList<WebElement> waitContentsElementToLoad(By by) {
-        getContent().wait.until(presenceOfAllElementsLocatedBy(by));
-        return (ArrayList) getContent().wait.until(visibilityOfAllElementsLocatedBy(by));
+  public WebElement waitContentElementToLoad(By by) {
+    return getWait().until(presenceOfElementLocated(by));
+  }
+
+  public boolean verifyLocale(Locale locale) {
+    this.getNavigation();
+
+    if (locale.equals(Locale.RU) && this.locale.getText().equals("English")) {
+      return true;
     }
 
-    public ArrayList<WebElement> waitAllNavElementsToLoad(By by) {
-        getNavigation().wait.until(presenceOfAllElementsLocatedBy(by));
-        return (ArrayList) getNavigation().wait.until(visibilityOfAllElementsLocatedBy(by));
-    }
-*/
-
-    public WebElement waitContentElementToLoad(By by) {
-        return getWait().until(presenceOfElementLocated(by));
+    if (locale.equals(Locale.ENG) && this.locale.getText().equals("Русский")) {
+      return true;
     }
 
-    public boolean verifyLocale(Locale locale) {
+    return false;
+  }
 
-        this.getNavigation();
-
-        if (locale.equals(Locale.RU) && this.locale.getText().equals("English")) {
-            return true;
-        }
-
-        if (locale.equals(Locale.ENG) && this.locale.getText().equals("Русский")) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public FramePage switchLocale() {
-        this.ensurePageLoaded().getNavigation().locale.click();
-        return this.ensurePageLoaded().getNavigation();
-    }
+  public FramePage switchLocale() {
+    this.ensurePageLoaded().getNavigation().locale.click();
+    return this.ensurePageLoaded().getNavigation();
+  }
 }
