4229b1dbd41e0bc31299d6db76df6273e00f0006
remitot <call9096463086@gmail.com>
2017-03-06 17:07:40 +0300

    refactor(JepRia): отказ от отдельного класса JepTypedColumn, функционал перенесен в метод JepColumn#createTypedColumn

--- App/src/java/com/technology/jep/jepria/client/widget/list/JepColumn.java
+++ App/src/java/com/technology/jep/jepria/client/widget/list/JepColumn.java
@@ -1,12 +1,19 @@
 package com.technology.jep.jepria.client.widget.list;
 
+import static com.technology.jep.jepria.shared.JepRiaConstant.DEFAULT_DATE_FORMAT;
+import static com.technology.jep.jepria.shared.JepRiaConstant.DEFAULT_TIME_FORMAT;
+
 import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.DateCell;
 import com.google.gwt.cell.client.NumberCell;
 import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.technology.jep.jepria.client.widget.list.cell.JepCheckBoxCell;
+import com.technology.jep.jepria.shared.field.JepTypeEnum;
 import com.technology.jep.jepria.shared.record.JepRecord;
 import com.technology.jep.jepria.shared.util.JepRiaUtil;
 
@@ -107,6 +114,40 @@ public class JepColumn<T, C> extends Column<T, C> {
     addClassName(COMMON_CELL_STYLE);
     if (wrapText) {
       addClassName(NORMAL_WRAP_STYLE);
+    }
+  }
+  
+  /**
+   * Создает столбец списка, типизированный JepTypeEnum'ом.
+   * @param fieldName идентификатор столбца (DB-поля)
+   * @param headerText заголовок столбца
+   * @param width ширина столбца
+   * @param type тип из таблицы:
+   * <table frame="border">
+   * <tr><td>Обычный текст</td><td>{@link JepTypeEnum#STRING}</td></tr>
+   * <tr><td>Время {@code 12:34:56}</td><td>{@link JepTypeEnum#TIME}</td></tr>
+   * <tr><td>Дата {@code 23.04.1987}</td><td>{@link JepTypeEnum#DATE}</td></tr>
+   * <tr><td>Дата и время {@code 23.04.1987 12:34:56}</td><td>{@link JepTypeEnum#DATE_TIME}</td></tr>
+   * <tr><td>Целое число {@code 1234}</td><td>{@link JepTypeEnum#INTEGER}</td></tr>
+   * <tr><td>Дробное число (1 знак) {@code 123,4} </td><td>{@link JepTypeEnum#DOUBLE} или {@link JepTypeEnum#FLOAT}</td></tr>
+   * <tr><td>Decimal-число {@code 1234,00}</td><td>{@link JepTypeEnum#BIGDECIMAL}</td></tr>
+   * <tr><td>Checkbox</td><td>{@link JepTypeEnum#BOOLEAN}</td></tr>
+   * </table>
+   */
+  public static JepColumn createTypedColumn(String fieldName, String headerText, double width, JepTypeEnum type) {
+    return new JepColumn(fieldName, headerText, width, createCell(type));
+  }
+
+  private static Cell<?> createCell(JepTypeEnum type) {
+    switch (type) {
+      case TIME: return new DateCell(DateTimeFormat.getFormat(DEFAULT_TIME_FORMAT));
+      case DATE: return new DateCell(DateTimeFormat.getFormat(DEFAULT_DATE_FORMAT));
+      case DATE_TIME: return new DateCell(DateTimeFormat.getFormat(DEFAULT_DATE_FORMAT + " " + DEFAULT_TIME_FORMAT));
+      case INTEGER: return new NumberCell(NumberFormat.getFormat("#"));
+      case DOUBLE: case FLOAT: return new NumberCell(NumberFormat.getFormat("#.#"));
+      case BIGDECIMAL: return new NumberCell(NumberFormat.getFormat("#0.00"));
+      case BOOLEAN: return new JepCheckBoxCell();
+      default: return new TextCell();
     }
   }
 
