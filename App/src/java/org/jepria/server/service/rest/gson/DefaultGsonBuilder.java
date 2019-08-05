9cf1ca85b30f82ba50bad81f813f07e3bf5cbac7
remitot <call9096463086@gmail.com>
2019-08-05 17:53:08 +0300

    refactor(JepRia): DefaultGsonBuilder implements Supplier<GsonBuilder>

--- App/src/java/org/jepria/server/service/rest/gson/DefaultGsonBuilder.java
+++ App/src/java/org/jepria/server/service/rest/gson/DefaultGsonBuilder.java
@@ -1,10 +1,17 @@
 package org.jepria.server.service.rest.gson;
 
+import java.util.function.Supplier;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class DefaultGsonBuilder {
+public class DefaultGsonBuilder implements Supplier<GsonBuilder> {
+  @Override
+  public GsonBuilder get() {
+    return new GsonBuilder().setPrettyPrinting();
+  }
+  
   public Gson build() {
-    return new GsonBuilder().setPrettyPrinting().create();
+    return get().create();
   }
 }
