e575c6f3c5f4c8e145f206fc12caad03a78ff566
remitot <call9096463086@gmail.com>
2019-11-22 11:09:51 +0300

    feat(Jepria): MetaInfoResource

---
+++ App/src/java/org/jepria/server/service/rest/MetaInfoResource.java
@@ -0,0 +1,54 @@
+package org.jepria.server.service.rest;
+
+import org.jepria.server.service.rest.gson.JsonConfig;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Path("/meta")
+public class MetaInfoResource extends JaxrsAdapterBase {
+
+  @GET
+  @Path("/current-user")
+  @JsonConfig(serializeNulls = true)
+  public Response getCurrentUser() {
+    Map<String, Object> responseBodyMap = new HashMap<>();
+    responseBodyMap.put("username", securityContext.getCredential().getUsername());
+    responseBodyMap.put("operatorId", securityContext.getCredential().getOperatorId());
+    return Response.ok(responseBodyMap).build();
+  }
+
+  @GET
+  @Path("/current-user/test-roles")
+  public Response testCurrentUserRoles(@QueryParam("roles") String roles) {
+    Map<String, Object> responseBodyMap = new LinkedHashMap<>();
+
+    if (roles != null) {
+      String[] tokens = roles.split("\\s*[,;]\\s*");
+      for (String role: tokens) {
+        if (!"".equals(role)) {
+          responseBodyMap.put(role, securityContext.isUserInRole(role) ? 1 : 0);
+        }
+      }
+    }
+
+    return Response.ok(responseBodyMap).build();
+  }
+
+  @GET
+  @Path("/current-user/test-role")
+  public Response testCurrentUserRole(@QueryParam("role") String role) {
+    Map<String, Object> responseBodyMap = new LinkedHashMap<>();
+
+    if (role != null && !"".equals(role)) {
+      responseBodyMap.put(role, securityContext.isUserInRole(role) ? 1 : 0);
+    }
+
+    return Response.ok(responseBodyMap).build();
+  }
+}
