8071673f7883ecb3892b25e13d3b9e0ae1b3e95c
remitot <call9096463086@gmail.com>
2019-12-31 14:16:26 +0300

    doc(Jepria): DaoSupport: объявление RuntimeSQLException и RuntimeException в javadoc

--- App/src/java/org/jepria/server/data/DaoSupport.java
+++ App/src/java/org/jepria/server/data/DaoSupport.java
@@ -14,20 +14,91 @@ public interface DaoSupport {
     return new DaoSupportOracle();
   }
 
+  /**
+   *
+   * @param query
+   * @param resultTypeClass
+   * @param params
+   * @param <T>
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   * @return
+   */
   <T> T create(String query, Class<? super T> resultTypeClass, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param params
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   */
   void execute(String query, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param mapper
+   * @param recordClass
+   * @param params
+   * @param <T>
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   * @return
+   */
   <T> List<T> find(String query, ResultSetMapper<? super T> mapper, Class<? super T> recordClass, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param resultTypeClass
+   * @param params
+   * @param <T>
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   * @return
+   */
   <T> T executeAndReturn(String query, Class<? super T> resultTypeClass, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param mapper
+   * @param modelClass
+   * @param params
+   * @param <T>
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   * @return
+   */
   <T> List<T> select(String query, ResultSetMapper<? super T> mapper, Class<? super T> modelClass, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param params
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   */
   void update(String query, Object... params);
 
+  /**
+   *
+   * @param query
+   * @param params
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   */
   void delete(String query, Object... params);
 
+  /**
+   *
+   * @param tableName
+   * @param dataFieldName
+   * @param whereClause
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   */
   void deleteClob(String tableName, String dataFieldName, String whereClause);
 
   /**
@@ -35,9 +106,19 @@ public interface DaoSupport {
    * @param dataFieldName field in the table which to upload object data to
    * @param whereClause must select a single row from the table, which to upload object data to
    * @param reader object data source. Passing {@code null} to erase the existing content is discouraged (may cause exceptions), use {@link #deleteClob(String, String, String)}
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   void uploadClob(String tableName, String dataFieldName, String whereClause, Reader reader);
 
+  /**
+   *
+   * @param tableName
+   * @param dataFieldName
+   * @param whereClause
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
+   */
   void deleteBlob(String tableName, String dataFieldName, String whereClause);
 
   /**
@@ -46,6 +127,8 @@ public interface DaoSupport {
    * @param dataFieldName field in the table which to upload object data to
    * @param whereClause must select a single row from the table, which to upload object data to
    * @param stream object data source. Passing {@code null} to erase the existing content is discouraged (may cause exceptions), use {@link #deleteBlob(String, String, String)}
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   void uploadBlob(String tableName, String dataFieldName, String whereClause, InputStream stream);
 
@@ -54,6 +137,8 @@ public interface DaoSupport {
    * @param dataFieldName
    * @param whereClause
    * @param content object data. Passing {@code null} (as well as passing {@code ""}) erases the existing content
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   default void uploadClob(String tableName, String dataFieldName, String whereClause, String content) {
     if (content == null || "".equals(content)) {
@@ -70,6 +155,8 @@ public interface DaoSupport {
    * @param whereClause
    * @param stream object data source. Passing {@code null} to erase the existing content is discouraged (may cause exceptions), use {@link #deleteClob(String, String, String)}
    * @param charset
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   default void uploadClob(String tableName, String dataFieldName, String whereClause, InputStream stream, String charset) {
     final Reader reader;
@@ -87,6 +174,8 @@ public interface DaoSupport {
    * @param dataFieldName field in the table which to upload object data to
    * @param whereClause must select a single row from the table, which to download object data from
    * @param writer object data target. If the object is {@code null} (as well as empty) in the database, no data will be written
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   // no charset parameter because the database already knows which charset it uses to store clob
   void downloadClob(String tableName, String dataFieldName, String whereClause, Writer writer);
@@ -97,6 +186,8 @@ public interface DaoSupport {
    * @param dataFieldName field in the table which to upload object data to
    * @param whereClause must select a single row from the table, download object data from
    * @param stream object data target
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    */
   void downloadBlob(String tableName, String dataFieldName, String whereClause, OutputStream stream);
 
@@ -105,6 +196,8 @@ public interface DaoSupport {
    * @param tableName
    * @param dataFieldName
    * @param whereClause
+   * @throws org.jepria.server.data.RuntimeSQLException в случае {@link java.sql.SQLException}
+   * @throws java.lang.RuntimeException в случае любого другого исключения
    * @return either non-empty string containing object data or {@code null} if the object is {@code null} (as well as empty) in the database
    */
   // no charset parameter because the database already knows which charset it uses to store clob
